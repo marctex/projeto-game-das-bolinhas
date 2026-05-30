a1ce3ead85b948f457f6315e39dad7fa06dbde37
Marcelo Teixeira Martins <marcelo.t.martins@hotmail.com>
2026-05-30 12:35:34 -0300

    criado script para obstaculos
    criado script para iniciar o game toda vez que player encostar nos sprites buraco, laser e giragira
    - criado prefabs

---
+++ ProjetoGameDaBolinha/Assets/Scripts/Obstacles.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Obstacles : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Gameover");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
