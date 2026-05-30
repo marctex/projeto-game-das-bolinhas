556a9e912b864f67b3ba5189dcf309f761ae97bf
Marcelo Teixeira Martins <marcelo.t.martins@hotmail.com>
2026-05-30 10:48:44 -0300

    inserido controle do player para Input system package
    utilizando o InputSystem_Actions e criado script PlayerMovement

---
+++ ProjetoGameDaBolinha/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speedPlayer = 5f;
+    private Rigidbody2D playerRB;
+    private Vector2 movementPlayer;
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerRB.linearVelocity = movementPlayer * speedPlayer;
+    }
+
+    private void OnMove(InputValue value)
+    {
+        movementPlayer = value.Get<Vector2>();
+    }
+}
